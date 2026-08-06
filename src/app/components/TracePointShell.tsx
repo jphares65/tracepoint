76e6c5d94615f146ea8e3cbb35e01d23170c1d4e
Jason Phares <YOUR_GITHUB_EMAIL>
2026-08-05 21:32:44 -0400

    Fix training navigation syntax

--- src/app/components/TracePointShell.tsx
+++ src/app/components/TracePointShell.tsx
@@ -1,4 +1,4 @@
-"use client";
+﻿"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -117,7 +117,6 @@ const NAV_ITEMS: NavigationItem[] = [
         "view_command_dashboard",
       ],
     },
-  },
   },
   {
     label: "Settings",
