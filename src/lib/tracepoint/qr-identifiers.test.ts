342fae475ffccbc5164fe54b46d612bb38a64af4
Jason Phares <YOUR_GITHUB_EMAIL>
2026-09-17 07:46:39 -0400

    feat: add fleet vehicle QR viewer

--- src/lib/tracepoint/qr-identifiers.test.ts
+++ src/lib/tracepoint/qr-identifiers.test.ts
@@ -5,6 +5,7 @@ import { buildTracePointQrValue } from "./qr-identifiers.ts";
 
 test("builds stable TracePoint vehicle and equipment QR identities", () => {
   assert.equal(buildTracePointQrValue("vehicle", "vehicle-123"), "tracepoint://vehicle/vehicle-123");
+  assert.equal(buildTracePointQrValue("vehicle", "vehicle-123"), "tracepoint://vehicle/vehicle-123");
   assert.equal(buildTracePointQrValue("equipment", "asset 42"), "tracepoint://equipment/asset%2042");
   assert.throws(() => buildTracePointQrValue("equipment", "   "), /record ID/);
 });
